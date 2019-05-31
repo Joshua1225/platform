1f72fe3a536ac01cae775d4f1bc01753f19e5192
Joshua <xiaodaiyue@foxmail.com>
2019-05-31 23:42:49 +0800

    add frontends Signed-off-by: Joshua <xiaodaiyue@foxmail.com>

---
+++ frontends/src/router.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Appeal from './views/Appeal.vue'
+import Login from './components/Login.vue'
+import AppealMessage from './components/AppealMessage.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    
+    {
+      path: '/',
+      name: 'home',
+      component: Appeal
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component:Login
+    },
+    {
+      path:'/appealMessage',
+      name:'appealMessage',
+      component:AppealMessage
+    }
+  ]
+})
