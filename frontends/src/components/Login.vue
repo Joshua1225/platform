e285c72422b22f493b784d0816e538b417aa68ef
Joshua <xiaodaiyue@foxmail.com>
2019-05-31 22:24:00 +0800

    add frontends dir for Vue project

---
+++ frontends/src/components/Login.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <button @click="change()">切换</button>
+    <form v-if="!isReg">
+      用户名：
+      <input type="text" v-model="name">
+      密码：
+      <input type="password" v-model="password">
+      <button type="button" @click="login()">登陆</button>
+      <button type="button" @click="reg()">注册</button>
+    </form>
+    <form v-else>
+      新用户名：
+      <input type="text" v-model="name">
+      新密码：
+      <input type="password" v-model="password">
+      再次输入密码
+      <input type="password" v-model="repeat">
+      <button type="button" @click="addUesr()">确定</button>
+      <button type="button" @click="cancel()">取消</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        isReg : false,
+        name: '',
+        password: '',
+        repeat: ''
+      }
+      
+    },
+    methods: {
+      login () {
+        this.$router.push('/')
+      },
+      reg () {
+        console.log("调用reg")
+        this.isReg = true
+      },
+      cancel () {
+        console.log("调用cancel")
+        this.isReg = false
+      },
+      change () {
+        console.log("调用change")
+        this.isReg = !this.isReg
+      },
+      addUesr (){
+        if(this.password === this.repeat){
+          localStorage.setItem("name",this.name)
+          localStorage.setItem("password",this.password)
+        }
+        else{
+          alert("")
+        }
+        
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontends/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <AppealMessagea/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/AppealMessage.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
