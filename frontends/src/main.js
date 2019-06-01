a821a2b54886313d4fc54944d5c31bf2fad0a083
Joshua <xiaodaiyue@foxmail.com>
2019-06-01 18:01:52 +0800

    Signed-off-by: Joshua <xiaodaiyue@foxmail.com>

--- frontends/src/main.js
+++ frontends/src/main.js
@@ -15,4 +15,4 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
-router.push("/Search")
+router.push("/")
--- frontends/src/views/Search.vue
+++ frontends/src/views/Search.vue
@@ -3,19 +3,19 @@
 <template>
   <div>
     <!-- Logo-->
-  <top/>
+
     <searchBar/>
-  <bottom/>
+
   </div>
 </template>
 <script>
-import Top from "@/components/Top.vue";
+
 import SearchBar from "@/components/SearchBar.vue";
-import Bottom from "@/components/Bottom.vue";
+
 export default {
   name: "search",
   components: {
-    SearchBar,Bottom,Top
+    SearchBar
   }
 };
 </script>
