9169e55434552cc1458bf82edfee5c65c8b76808
your catkin <1169333043@qq.com>
2019-06-01 14:06:51 +0800

    删除了重复文件 统一了部分变量名

--- backends/views.py
+++ backends/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
-from .forms import UploadFileForm, AcademiaForm
-from .models import  Unidentified_Academia
+from .forms import AcademiaForm, UploadFileForm
+from backends.models import UnidentifiedAcademia
 
 
 def test(request):
@@ -59,7 +59,7 @@ def academia_edit(request):
         先把已有的用户信息读出来，然后判断用户请求是POST还是GET。如果是GET，则显示表单
         并将用户已有信息也显示在其中，如果是POST，则接收用户提交的表单信息，然后更新各个数据模型实例属性的值
     '''
-    academia = Unidentified_Academia.objects.get(id = request.user.academia_id)  #这里是默认发送了user的信息
+    academia = UnidentifiedAcademia.objects.get(id = request.user.academia_id)  #这里是默认发送了user的信息
     if request.method == "POST":
         academia_form = AcademiaForm(request.POST)
         if academia_form.is_valid():
@@ -72,4 +72,4 @@ def academia_edit(request):
         return HttpResponseRedirect('填写你要跳转的url')
     else:
         academia_form = AcademiaForm(instance=request.user)
-        return render(request,"填写你要跳转的html", {"user_form":academia_form})
+        return render(request, "填写你要跳转的html", {"user_form":academia_form})
