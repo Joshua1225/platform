ee0db545a35f94fcbed4726eb2a80ac923faa135
Ku3agi <mingzhentan10@163.com>
2019-06-01 18:02:06 +0800

    Signed-off-by: Ku3agi <mingzhentan10@163.com>

--- frontends/src/views/Search.vue
+++ frontends/src/views/Search.vue
@@ -3,19 +3,15 @@
 <template>
   <div>
     <!-- Logo-->
-  <top/>
     <searchBar/>
-  <bottom/>
   </div>
 </template>
 <script>
-import Top from "@/components/Top.vue";
 import SearchBar from "@/components/SearchBar.vue";
-import Bottom from "@/components/Bottom.vue";
 export default {
   name: "search",
   components: {
-    SearchBar,Bottom,Top
+    SearchBar
   }
 };
 </script>
