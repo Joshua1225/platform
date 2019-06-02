83c8fca8e90b72d24befbf7f7278b914af56f09b
PeinAkatsuki <534079292@qq.com>
2019-06-02 20:46:41 +0800

    Signed-off-by: PeinAkatsuki <534079292@qq.com>

--- frontends/src/views/User.vue
+++ frontends/src/views/User.vue
@@ -9,19 +9,35 @@
 
 <template>
   <div class="userouter">
-    <userinfo />
+      <el-tabs v-model= "chosen" type="card" @tab-click="handleClick">
+    <el-tab-pane label="我的信息" name="first">
+      <userinfo />
     <guessyoulike />
-
+    </el-tab-pane>
+    <el-tab-pane label="我的关注" name="second">我的关注</el-tab-pane>
+    <el-tab-pane label="我的消息" name="third">我的消息</el-tab-pane>
+    <el-tab-pane label="我的论文" name="fourth">
+      <mypapers/>
+    </el-tab-pane>
+  </el-tabs>
   </div>
 </template>
 <script>
 import userinfo from "@/components/userinfo.vue";
 import guessyoulike from "@/views/guessyoulike";
+import mypapers from "@/views/mypapers";
 export default {
   name: "user",
   components: {
     userinfo,
-    guessyoulike
+    guessyoulike,
+    mypapers
+  },
+  data: function()
+  {
+    return {
+      chosen:"first",
+    }
   }
 };
 </script>
