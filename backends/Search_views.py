cfe74d607b081d93761b52e38efa4ebe7ad1540e
catkin <1169333043@qq.com>
2019-06-03 19:09:39 +0800

    重载了修改在源码的函数

--- backends/Search_views.py
+++ backends/Search_views.py
@@ -1,5 +1,6 @@
 from haystack.views import SearchView
 import json
+from django.core.paginator import InvalidPage, Paginator
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
@@ -21,6 +22,33 @@ class MySearchView(SearchView):
 
         return self.form_class(data, **kwargs)
 
+    def build_page(self):
+        """
+        Paginates the results appropriately.
+
+        In case someone does not want to use Django's built-in pagination, it
+        should be a simple matter to override this method to do what they would
+        like.
+        """
+        try:
+            page_no = int(self.request.GET.get('page', 1))
+        except (TypeError, ValueError):
+            raise Http404("Not a valid number for page.")
+
+        if page_no < 1:
+            raise Http404("Pages should be 1 or greater.")
+
+        start_offset = (page_no - 1) * self.results_per_page
+        self.results[start_offset:start_offset + self.results_per_page]
+
+        paginator = Paginator(self.results, self.results_per_page)
+
+        try:
+            page = paginator.page(page_no)
+        except InvalidPage:
+            raise Http404("No such page!")
+        return (paginator, page)
+
     def create_response(self):
         """
         Generates the actual HttpResponse to send back to the user.
@@ -30,4 +58,6 @@ class MySearchView(SearchView):
         data = self.results.values("title", "author", "keywords", "year", "n_citation", "language")
         resp = json.dumps(list(data))
         return HttpResponse(resp)
+
+
 
