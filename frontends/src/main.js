fb51c402cddf011a3d231c772dddcf7f64d5d1f5
Joshua <xiaodaiyue@foxmail.com>
2019-06-03 14:49:40 +0800

    Signed-off-by: Joshua <xiaodaiyue@foxmail.com>

--- frontends/src/main.js
+++ frontends/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import top from '@/components/Top.vue'
 
@@ -21,5 +22,5 @@ new Vue({
 }).$mount('#app')
 //router.push("/Search")
 
-router.push("/")
+router.push("/papers")
 
